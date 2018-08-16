e39eb20d6c12d1833e1e976ed1226f14b0214448
juniorsolo <junior.solo@hotmail.com>
2018-08-15 21:56:44 -0300

    Criando serviço com spring para o envio de email.

---
+++ src/main/java/br/com/enviaemail/EnviaEmail/EmailService.java
@@ -0,0 +1,30 @@
+package br.com.enviaemail.EnviaEmail;
+
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+	
+	public void enviar(String nome, String emailDestinatario) {
+		try {
+			Email email = new SimpleEmail();
+			email.setHostName("smtp.googlemail.com");
+			email.setSmtpPort(465);
+			email.setAuthentication("junior.r.solo@gmail.com", "agu972540");
+			email.setSSLOnConnect(true);
+			
+			email.setFrom("junior.solo@hotmail.com");
+			email.setSubject("Você foi convidado pelo ListaVIP");
+			email.setMsg("Olá " + nome + "você acaba de ser convidado pelo ListaVip.");
+			email.addTo(emailDestinatario);
+			email.send();
+		}catch (Exception e) {
+		 e.getStackTrace();
+		}
+		
+	}
+}
---
+++ src/main/java/br/com/enviaemail/EnviaEmail/EnviaEmailApplication.java
@@ -0,0 +1,12 @@
+package br.com.enviaemail.EnviaEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviaEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviaEmailApplication.class, args);
+	}
+}
